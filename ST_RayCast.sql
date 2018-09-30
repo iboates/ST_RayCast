39419ad0e4bf837f4134c828777b2b74c6bea717
Isaac Boates <iboates@gmail.com>
2018-09-30 15:04:26 +0200

    added test data

--- ST_RayCast.sql
+++ ST_RayCast.sql
@@ -1,9 +1,9 @@
 ﻿CREATE OR REPLACE FUNCTION ST_RayCast(
 	in_point GEOMETRY,
 	in_boundaries GEOMETRY,
-	out_geom_type TEXT DEFAULT 'POINT',
-	num_rays INTEGER DEFAULT 32,
-	max_ray_dist FLOAT DEFAULT 1000
+	out_geom_type TEXT,
+	num_rays INTEGER,
+	max_ray_dist FLOAT
 )
 
 RETURNS GEOMETRY AS
@@ -22,6 +22,10 @@ BEGIN
 	/* Do preliminary checks for common problems */
 	IF ST_SRID(in_boundaries) != srid THEN
 		RAISE EXCEPTION 'SRID of input points (%) does not match input boundaries SRID (%)', ST_SRID(in_point), ST_SRID(in_boundaries);
+	END IF;
+
+	IF out_geom_type NOT IN ('MULTIPOINT', 'MULTILINESTRING') THEN
+		RAISE EXCEPTION 'Output geometry type (''%'') must be one of (''%'', ''%'')', out_geom_type, 'MULTIPOINT', 'MULTILINESTRING';
 	END IF;
 
 	/* Cast rays over the specified angle window */
@@ -90,9 +94,9 @@ BEGIN
 		END IF;
 
 		/* Either prep the point for return or make a line out of it for return depending on user input */
-		IF out_geom_type = 'POINT' THEN
+		IF out_geom_type = 'MULTIPOINT' THEN
 			return_geom = ST_Collect(ST_CollectionExtract(return_geom, 1), candidate_geom);
-		ELSIF out_geom_type = 'LINESTRING' THEN
+		ELSIF out_geom_type = 'MULTILINESTRING' THEN
 			return_geom = ST_Collect(
 				ST_CollectionExtract(
 					return_geom,
@@ -107,30 +111,19 @@ BEGIN
 	END LOOP;
 
 	/* Return all the points or lines created */
-	IF out_geom_type = 'POINT' THEN
+	IF out_geom_type = 'MULTIPOINT' THEN
 		RETURN ST_Multi(ST_CollectionExtract(return_geom, 1));
-	ELSIF out_geom_type = 'LINESTRING' THEN
+	ELSIF out_geom_type = 'MULTILINESTRING' THEN
 		RETURN ST_Multi(ST_CollectionExtract(return_geom, 2));
 	END IF;
 
 END
 $$
 LANGUAGE plpgsql;
-
-DROP TABLE IF EXISTS multi_rays;
-CREATE TABLE multi_rays AS (
-	SELECT
-		p.id AS p_id,
-		(SELECT ST_RayCast(
-			p.geom,
-			ST_CollectionExtract(ST_Collect(e.geom), 2),
-			out_geom_type := 'POINT',
-			num_rays := 8,
-			max_ray_dist := 150
-		)) AS geom
-	FROM 
-		point AS p,
-		edge AS e
-	GROUP BY
-		p.id
-)
+COMMENT ON FUNCTION ST_RayCast(
+	GEOMETRY,
+	GEOMETRY,
+	TEXT,
+	INTEGER,
+	FLOAT
+) IS 'Created by Isaac Boates. Use of this software is at the user''s own risk, and no responsibility is claimed by the creator in the event of damages, whether tangible or financial caused directly or indirectly by the use of this software.';
